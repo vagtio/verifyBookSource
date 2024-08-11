606e31e0c75ffddd2842d5245add8fa1651c29d1
vägtio <57786594+vagtio@users.noreply.github.com>
2024-08-11 21:56:43 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,24 @@
+import os
 import json
-import os.path
 import time
+import sys
 from book.book import BookSourceManager
 
 
-def main():
-    """主函数，处理用户输入并执行书源处理流程"""
-    print("欢迎使用书源校验工具（VerifyBookSource v2.2）\n"
-          "作者：勿埋我心 - SkyQian\n"
-          "Github：https://github.com/Qiantigers/verifyBookSource\n"
-          "我的博客：https://www.skyqian.com\n"
-          f"{'-' * 16}")
+def resource_path(relative_path):
+    """ 获取资源的绝对路径，适用于开发环境和 PyInstaller 打包后的环境 """
+    try:
+        # PyInstaller 创建临时文件夹并将路径存储在 _MEIPASS 中
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+    return os.path.join(base_path, relative_path)
 
-    config_path = 'book/config.json'
+
+def load_or_create_config():
+    """ 加载或创建配置文件 """
+    config_path = resource_path('book/config.json')
     if input('是否使用config.json文件？（不使用则通过命令行输入配置）（y/n）').lower() == 'n':
-        # 通过命令行输入配置
         config = {
             'path': input('本地文件路径/文件直链URL：'),
             'outpath': input('书源输出路径（为空则为当前目录，目录最后带斜杠）：') or './',
@@ -24,32 +28,37 @@ def main():
             'keywords_to_filter': input('请输入要过滤的关键词，用逗号分隔：').split(',') if input(
                 '是否过滤特定关键词的书源？（y/n）').lower() == 'y' else []
         }
-        # 保存配置到文件
         with open(config_path, 'w', encoding='utf-8') as f:
             json.dump(config, f, ensure_ascii=False, indent=4, sort_keys=False)
     else:
-        # 从文件加载配置
         if not os.path.exists(config_path):
             print('config.json文件不存在，请检查一下。或者使用命令行输入配置。')
-            return
+            return None
         with open(config_path, 'r', encoding='utf-8') as f:
             config = json.load(f)
+    return config
 
-    # 创建BookSourceManager实例
+
+def main():
+    print("欢迎使用书源校验工具（VerifyBookSource v2.1）\n"
+          "作者：勿埋我心 - SkyQian\n"
+          "Github：https://github.com/Qiantigers/verifyBookSource\n"
+          "我的博客：https://www.skyqian.com\n"
+          f"{'-' * 16}")
+
+    config = load_or_create_config()
+    if config is None:
+        return
+
     manager = BookSourceManager(config['path'], config)
 
-    # 开始处理书源
     start_time = time.time()
     results = manager.process_books(int(config['workers']))
     elapsed_time = time.time() - start_time
 
-    # 保存结果
     manager.save_results(results, config['outpath'])
 
-    # 分析结果
     analysis = manager.analyze_results(results)
-
-    # 打印报告
     print(f"\n{'-' * 16}\n"
           "成果报表\n"
           f"书源总数：{analysis['total']}\n"
